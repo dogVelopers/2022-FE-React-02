9fe254bd4f70c32f26b716f0b524aa4f0aa4fbe9
NohWookJin <jinn98@kakao.com>
2022-05-11 20:19:31 +0900

    FE3 과제완료(노욱진)
    FE3 과제완료(노욱진)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
-import React from "react";
+import "./app.css";
+
+import React, { useState, useEffect } from "react";
 
 function App() {
+  let [number, setNumber] = useState<number[]>([0, 1, 2]);
+
   return (
     <div>
-      <h1>hello dogvelopers</h1>
+      <div className="numBoxContainer">
+        {number.map((i) => {
+          return (
+            <div className="numBox" key={i}>
+              {number[i]}
+            </div>
+          );
+        })}
+      </div>
+      <div className="buttonDiv">
+        <button
+          onClick={() => {
+            const plusNum = [...number];
+            let newNumLength = plusNum.length;
+            plusNum.push(newNumLength);
+            setNumber(plusNum);
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            const minusNum = [...number];
+            minusNum.pop();
+            setNumber(minusNum);
+          }}
+        >
+          -
+        </button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// 3. 더하기 버튼을 눌렀을 때 “현재 배열의 길이” 값을 배열의 마지막에 추가
+// 4. 빼기 버튼을 눌렀을 때 “가장 오른쪽 마지막" 값을 배열에서 삭제
