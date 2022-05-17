13e535639b4dfeb4d5d0aff4e6ca95cacbc34d3f
NohWookJin <jinn98@kakao.com>
2022-05-17 16:10:01 +0900

    과제 - 노욱진
    과제 - 노욱진

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,52 @@
-import React from "react";
+import "./app.css";
+
+import React, { useState, ChangeEvent } from "react";
 
 function App() {
+  const [dan, setDan] = useState<number[] | any>([2, 3, 4, 5, 6, 7, 8, 9]);
+  const [num, setNum] = useState<number[]>([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+  function onChange(event: ChangeEvent<HTMLSelectElement>) {
+    setDan(event.target.value);
+  }
+
+  const newDan = [...num];
+  newDan.shift();
+
   return (
     <div>
-      <h1>hello dogvelopers</h1>
+      <h1>{dan[0]}단</h1>
+
+      <select onChange={onChange}>
+        {newDan.map((i: any) => (
+          <option value={newDan[i]}>{newDan[i]}</option>
+        ))}
+      </select>
+
+      <div>
+        <Component dan={dan} num={num} />
+      </div>
+    </div>
+  );
+}
+
+interface Props {
+  dan: number[];
+  num: number[];
+}
+
+function Component({ dan, num }: Props) {
+  return (
+    <div>
+      {num.map((i, index) => {
+        return (
+          <div key={index}>
+            <span>{dan[0]} </span>
+            <span>{num[i]} </span>
+            <span>{dan[0] * num[i]}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
