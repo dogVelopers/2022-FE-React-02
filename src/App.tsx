5ac8a799001f3caa87692e72fb5cd24042f44f4e
Hyesung Oh <haesungoh414@gmail.com>
2022-05-19 14:10:41 +0900

    onSubmit, onChange 강의

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 function App() {
+  // onSubmit > preventDefault
+  function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    console.log(todo);
+    setTodo("");
+  }
+
+  // state for input
+  const [todo, setTodo] = useState<string>("");
+
+  // onChange
+  function onChange(e: ChangeEvent<HTMLInputElement>) {
+    setTodo(e.target.value);
+  }
+
+  // state binding > onChange setState > onSubmit
+
   return (
     <div>
-      <h1>hello dogvelopers</h1>
+      <h1>submit과 change</h1>
+
+      <form onSubmit={onSubmit}>
+        <input value={todo} onChange={onChange} />
+        <button type="submit">확인</button>
+      </form>
+
+      <section>
+        <article></article>
+      </section>
     </div>
   );
 }
