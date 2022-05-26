c2d4d33cb719b2629711170e3ab8d4cc123e746b
seulhee <3021062@gmail.com>
2022-05-26 14:28:25 +0900

    feat: 피드백 수정

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -2,38 +2,46 @@ import { ChangeEvent, FormEvent, useState } from "react";
 import "./Todo.css";
 
 interface ITodo {
-    id: number;
-    summary: string;
+  id: number;
+  summary: string;
 }
 
-
 function Todo() {
-    const [todos, setTodos] = useState<ITodo[]>([]);
-    const [newTodos, setNewTodos] = useState<string>("");
+  const [todo, setTodo] = useState<ITodo[]>([]);
+  const [newTodo, setNewTodo] = useState<string>("");
 
-    function onSubmitForm(event: FormEvent<HTMLFormElement>) {
-        event?.preventDefault();
-        setTodos(prev => [...prev, {id: prev.length, summary: newTodos}])
-        setNewTodos("");
-    }
+  function onSubmitForm(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setTodo((prev) => [...prev, { id: prev.length, summary: newTodo }]);
+    setNewTodo("");
+  }
 
-    function onChangeInput(event: ChangeEvent<HTMLInputElement>) {
-        setNewTodos(Object(event.target.value));
-    }
-    return (
-        <div className="centerMockup">
-            <h1>오늘 할일</h1>
-            <form onSubmit={onSubmitForm}>
-                <label htmlFor="id">💭</label>
-                <input id="id" placeholder="할일을 입력하고 엔터를 누르세요." className="todoInput" value={newTodos} onChange={onChangeInput} />
-            </form>
-            <ul className="listUl">
-                {todos.map((todo: ITodo) =>
-                    <li key={todo.id} className="list"><input type="checkbox"/>{todo.summary}</li>
-                )}
-            </ul>
-        </div>
-    )
+  function onChangeInput(event: ChangeEvent<HTMLInputElement>) {
+    setNewTodo(event.target.value);
+  }
+  return (
+    <div className="centerMockup">
+      <h1>오늘 할일</h1>
+      <form onSubmit={onSubmitForm}>
+        <label htmlFor="id">💭</label>
+        <input
+          id="id"
+          placeholder="할일을 입력하고 엔터를 누르세요."
+          className="todoInput"
+          value={newTodo}
+          onChange={onChangeInput}
+        />
+      </form>
+      <ul className="listUl">
+        {todo.map((todo: ITodo) => (
+          <li key={todo.id} className="list">
+            <input type="checkbox" />
+            {todo.id}번 {todo.summary}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Todo;
